885f75415626b00d7f2187b1333d4b69aaf6601a
Nowfeel <n.safi@groupecerise.fr>
2022-03-16 11:26:04 +0100

    calculatrice terminée

--- script.js
+++ script.js
@@ -2,51 +2,84 @@
 let a = 0
 let b = 0 
 let c = 0 
-
-// choix de l'opération 
-let choix = prompt('1: addition /n 2: soustraction 3: multiplication 4: division');
+let choix = 0;
 
 // addition 
 function addition(){
-    a = parseInt(prompt('choix nombre 1'))
-    b = parseInt(prompt('choix nombre 2'))
+    a = parseInt(prompt('ADDITION \n \n Choisis ton premier nombre : \n'))
+    b = parseInt(prompt('ADDITION \n \n Choisis ton deuxième nombre : \n'))
     c = a + b;
-    alert('Le résultat est : ' + c);
+    if (isNaN(c)) {
+        alert('je ne peux pas effectuer d\'opération si je n\'ai pas deux nombres, rafraichis la page')
+    } else {
+        alert('Le résultat est : ' + c);
+    }
+    return c;
 }
 
-// soustraction 
+// soustraction
 function soustraction(){
-    a = parseInt(prompt('choix nombre 1'))
-    b = parseInt(prompt('choix nombre 2'))
-    c = (a - b);
-    alert('Le résultat est : ' + c);
+    a = parseInt(prompt('SOUSTRACTION \n \n Choisis ton premier nombre : \n'))
+    b = parseInt(prompt('SOUSTRACTION \n \n Choisis ton deuxième nombre : \n '))
+    c = a - b;
+    if (isNaN(c)) {
+        alert('je ne peux pas effectuer d\'opération si je n\'ai pas deux nombres, rafraichis la page')
+    } else {
+        alert('Le résultat est : ' + c);
+    }
+    return c;
 }
 
 // multiplication 
 function multiplication(){
-    a = parseInt(prompt('choix nombre 1'))
-    b = parseInt(prompt('choix nombre 2'))
-    c = a * b;
-    alert('Le résultat est : ' + c);
+    a = parseInt(prompt('MULTIPLICATION \n \n Choisis ton premier nombre : \n'))
+    b = parseInt(prompt('MULTIPLICATION \n \n Choisis ton deuxième nombre : \n'))
+    c =  a * b;
+    if (isNaN(c)) {
+        alert('je ne peux pas effectuer d\'opération si je n\'ai pas deux nombres, rafraichis la page')
+    } else {
+        alert('Le résultat est : ' + c);
+    }
+    return c;
 }
 
 // division 
 function division(){
-    a = parseInt(prompt('choix nombre 1'))
-    b = parseInt(prompt('choix nombre 2'))
-    c = a / b;
-    alert('Le résultat est : ' + c);
+    a = parseInt(prompt('DIVISION \n \n Choisis ton premier nombre : \n'))
+    b = parseInt(prompt('DIVISIONs \n \n Choisis ton deuxième nombre : \n'))
+
+    if( b ==  0) {
+        alert('je ne peux diviser par 0 \n \nRafraichir la page')
+    } else {
+        c = a / b;
+        if (isNaN(c)) {
+            alert('je ne peux pas effectuer d\'opération si je n\'ai pas deux nombres, rafraichis la page')
+        } else {
+            alert('Le résultat est : ' + c);
+        }
+        return c;
+    }
 }
 
-// s'assurer de choisir un des choix et pas autre chose 
+// choix de l'opération 
+do {
+    choix = parseInt(prompt('Fais ton choix (sélectionne l\'opération en tapant sa valeur):\n\n 1 - Addition\n 2 - soustraction\n 3 - multiplication \n 4 - division'));
+} while (choix > 4 || isNaN(choix)) 
 
-// comportement en fonction du choix 
-if (choix == 1) {
-    addition();
-} else if(choix == 2) {
-    soustraction();
-} else if(choix == 3) {
-    multiplication();
-} else if(choix == 4) {
-    division();
-};
+// comportement en fonction du choix + obligation de choisir nombre 
+switch(choix) {
+    case 1:
+        addition();
+    break;
+    case 2:
+        soustraction();
+    break;
+    case 3:
+        multiplication();
+    break;
+    case 4: 
+        division();
+    break;
+    default: 
+        alert('erreur')
+}
