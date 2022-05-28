efc5299d8c69119222904a2f086ef332afc9f8de
Nowfeel Safi <noz@macbook-air-de-nowfeel.home>
2022-05-28 18:02:50 +0200

    morpion tuto starting

---
+++ react-morpion/src/index.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+// 1er composant 
+class Square extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null,
+        };
+    }
+
+    render() {
+    return (
+        <button 
+        className="square" 
+        onClick={ () => this.setState({value : 'X'})}>
+            {this.state.value}
+        </button>
+    );
+    }
+}
+
+//2e composant 
+class Board extends React.Component {
+    renderSquare(i) {
+    return <Square value={i}/>;
+    }
+
+    render() {
+    const status = 'Next player: X';
+
+    return (
+        <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+            {this.renderSquare(3)}
+            {this.renderSquare(4)}
+            {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+        </div>
+    </div>
+    );
+    }
+}
+
+//3e composant 
+class Game extends React.Component {
+    render() {
+    return (
+        <div className="game">
+        <div className="game-board">
+            <Board />
+        </div>
+        <div className="game-info">
+            <div>{/* status */}</div>
+            <ol>{/* TODO */}</ol>
+        </div>
+        </div>
+    );
+    }
+}
+
+  // ========================================
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Game />);
